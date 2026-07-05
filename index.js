135dee163350ada71b9522f6fc26de631a1e4a3f
NaylaSofiaB <nsbs1@aluno.ifnmg.edu.br>
2026-07-05 15:19:28 +0000

    feat: implementa CRUD de mensagens com Prisma

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'; // importa o Express usando ES Modules
 import logger from './middlewares/logger.js';      // importa o middleware de log
 import alunosRouter from './routes/alunos.js';
+import mensagensRouter from './routes/mensagens.js';
 
 const app = express(); // cria a aplicação Express
 const PORT = 3000; // porta onde o servidor vai rodar localmente
@@ -18,6 +19,7 @@ app.get('/status', (req, res) => {
 })
 
 app.use('/alunos', alunosRouter);
+app.use('/mensagens', mensagensRouter);
 
 // inicia o servidor localmente — na Vercel essa parte é pulada
 if (process.env.VERCEL !== '1') {
